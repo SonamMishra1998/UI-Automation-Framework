68eda4b877825593f7a3f03d16fe22bbcf2bcb53
sonamm <mishrasonam907@gmail.com>
2025-11-09 13:33:58 +0530

    commit logs and report changes

--- src/test/java/com/ui/testscripts/loginTest.java
+++ src/test/java/com/ui/testscripts/loginTest.java
@@ -3,19 +3,25 @@ package com.ui.testscripts;
 import static com.ui.utility.constants.Browser.CHROME;
 import static org.testng.Assert.assertEquals;
 
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.ui.dataProviders.LoginDataProvider;
 import com.ui.pages.HomePage;
 import com.ui.pojos.Users;
-
+import com.ui.utility.LoggerUtil;
+import com.ui.utility.constants.Environment;
+import com.ui.utility.readers.PropertyReader;
+@Listeners({com.ui.listeners.TestListener.class})
 public class loginTest {
 	HomePage home;
+	Logger logger=LoggerUtil.getlogger(this.getClass());
 
 	@BeforeTest
 	public void setUp() {
-		// setProperties(Environment.QA );
+		PropertyReader.setProperties(Environment.QA );
 
 		home = new HomePage(CHROME);
 	}
@@ -35,10 +41,11 @@ public class loginTest {
 			"regression" }, dataProviderClass = LoginDataProvider.class, dataProvider = "loginCSVDataProvider",retryAnalyzer=com.ui.listeners.RetryAnalyzer.class)
 
 	public void loginwithCsvData(Users user) {
-		System.out.println("start in login test");
+	//	logger.info("start in login test");
+
 		home = new HomePage(CHROME);
 		assertEquals(home.signIn().login(user.getUsername(), user.getPassword()).verifyName(), "Sonam mishra");
-
+	//	logger.info("end in login test");
 	}
 
 }
