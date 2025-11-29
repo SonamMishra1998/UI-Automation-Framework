7d645c5ecb06ff334e66619d76606de8d8608e54
sonamm <mishrasonam907@gmail.com>
2025-11-29 18:38:47 +0530

    feat: Add logging, screenshots, retry mechanism, LambdaTest integration, Surefire support & headless mode
    - Implemented centralized framework logging
    - Added screenshot capture on test failures
    - Integrated TestNG retry mechanism for flaky tests
    - Added LambdaTest remote execution support
    - Configured Maven Surefire plugin for terminal-based test execution
    - Enabled headless mode for Chrome/Firefox for CI/CD runs

--- src/test/java/com/ui/testscripts/LoginTest.java
+++ src/test/java/com/ui/testscripts/LoginTest.java
@@ -1,31 +1,15 @@
 package com.ui.testscripts;
 
-import static com.ui.utility.constants.Browser.CHROME;
 import static org.testng.Assert.assertEquals;
 
-import org.apache.logging.log4j.Logger;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.ui.dataProviders.LoginDataProvider;
-import com.ui.pages.HomePage;
 import com.ui.pojos.Users;
-import com.ui.utility.LoggerUtil;
-import com.ui.utility.constants.Environment;
-import com.ui.utility.readers.PropertyReader;
-@Listeners({com.ui.listeners.TestListener.class})
-public class loginTest {
-	HomePage home;
-	Logger logger=LoggerUtil.getlogger(this.getClass());
 
-	@BeforeTest
-	public void setUp() {
-		PropertyReader.setProperties(Environment.QA );
-
-		home = new HomePage(CHROME);
-	}
-
+@Listeners({ com.ui.listeners.TestListener.class })
+public class LoginTest extends TestBase {
 //	@Test(description = "This test Method is used to verify the valid user able to login into the application", groups = {
 //			"smoke", "sanity", "regression" },dataProviderClass=LoginDataProvider.class,dataProvider="login")
 //
@@ -35,17 +19,15 @@ public class loginTest {
 //	assertEquals(home.signIn().login(user.getUsername(), user.getPassword()).verifyName(),"Sonam mishra");
 //
 //	
-//	}
+//}
 	@Test(description = "This test Method is used to verify the valid user able to login into the application", groups = {
 			"smoke", "sanity",
-			"regression" }, dataProviderClass = LoginDataProvider.class, dataProvider = "loginCSVDataProvider",retryAnalyzer=com.ui.listeners.RetryAnalyzer.class)
+			"regression" }, dataProviderClass = LoginDataProvider.class, dataProvider = "loginCSVDataProvider", retryAnalyzer = com.ui.listeners.RetryAnalyzer.class)
 
 	public void loginwithCsvData(Users user) {
-	//	logger.info("start in login test");
-
-		home = new HomePage(CHROME);
+		logger.info("start in login test");
 		assertEquals(home.signIn().login(user.getUsername(), user.getPassword()).verifyName(), "Sonam mishra");
-	//	logger.info("end in login test");
+		logger.info("end in login test");
 	}
 
 }
